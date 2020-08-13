6894c1f15d265c608d747ec0d121dfc38753ff8b
Hawraa Almasoodi <67607847+HawraaAlmasoodi@users.noreply.github.com>
2020-08-13 10:39:11 +0200

    Add files via upload

---
+++ Eindopdracht-M03-KleurenToggle/script.js
@@ -0,0 +1,34 @@
+let toggleNavStatus = false;
+let navList = document.querySelector(".nav-sidebar");
+let btnMenuElement = document.getElementById("btn-menu");
+btnMenuElement.addEventListener('click', function () {
+    if (toggleNavStatus == false) {
+        navList.style.visibility = "visible";
+        navList.style.width = "358px";
+        toggleNavStatus = true;
+    } else if (toggleNavStatus == true) {
+        navList.style.visibility = "hidden";
+        navList.style.width = 0;
+        toggleNavStatus = false;
+    }
+});
+
+let nameColor = document.getElementById("colornamelabel");
+let listItems = document.getElementsByClassName("listitems");
+let radioBtns = document.getElementsByTagName('input');
+Array.from(listItems).forEach(function (listitem) {
+    listitem.addEventListener('click', function () {
+        document.body.style.backgroundColor = listitem.id;
+        navList.style.visibility = "hidden";
+        nameColor.style.visibility = "visible";
+        nameColor.innerHTML = listitem.id;
+        toggleNavStatus = false;
+        let btnId = "btn-" + listitem.id;
+        document.getElementById(btnId).checked = true;
+    })
+});
+
+let listItemsBackgroundColor = document.getElementsByTagName("li");
+for (let i = 0; i < listItemsBackgroundColor.length; i++) {
+    listItemsBackgroundColor[i].style.backgroundColor = listItemsBackgroundColor[i].id;
+}
